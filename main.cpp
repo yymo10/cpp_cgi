f24f945a34781cb170b0a79f3b6e690d4e7bd715
山本陽平 <y-yamamoto@infic.net>
2024-02-09 12:21:52 +0900

    main.cppサンプルコード追加

--- main.cpp
+++ main.cpp
@@ -1,20 +1,15 @@
 #include "lib/cgi.h"
 #include<iostream>
 #include<string>
-int main() {
-    // コンテンツタイプのヘッダを出力
-    std::cout << "Content-type: text/html\n\n";
-
-    // GETデータを取得してパース
-    auto get_data = parseQueryString();
-
-    // 取得したデータをHTML形式で出力
-    std::cout << "<html><body>";
-    std::cout << "<h1>GET Data</h1>";
-    for (auto& pair : get_data) {
-        std::cout << "<p>" << pair.first << " = " << pair.second << "</p>";
-    }
-    std::cout << "</body></html>";
-
+int main() { 
+    CGI cgi;
+    HtmlHeader("ja","UTF-8","テスト");
+    v("<h1>GET Data</h1>");
+    v(cgi.GET_("test"));
+   // v(cgi.POST_("test"));
+    v(cgi.REQUEST_("test"));
+    v(cgi.SESSION_ID());
+    v("<form action='./main-out.cgi' method='post'><input type='text' name='test' value='thello'></input><input type='submit' value='send'></input> </form>");
+    HtmlFooter();
     return 0;
 }
