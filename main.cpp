cca82efbfbdf72f842595e58f8388fefbeaa2565
YoheiYamamoto <y.yamamoto.infic2@gmail.com>
2024-02-21 21:17:13 +0900

    リファラ確認用リンク追加

--- main.cpp
+++ main.cpp
@@ -8,8 +8,11 @@ int main() {
         v(cgi.GET_("test"));
         v(cgi.REQUEST_("test"));
         v(cgi.SESSION_ID());
+        v(cgi.REFERER_());
         v(cgi.error_msg());
         v(cgi.status);
+
+        v("<a href='http://localhost:8888/cgi-bin/main-out.cgi'>Ref</a>");
         v("<form action='./main-out.cgi' method='post'><input type='text' name='test' value='thello'></input><input type='submit' value='send'></input> </form>");
     HtmlFooter();
     return 0;
